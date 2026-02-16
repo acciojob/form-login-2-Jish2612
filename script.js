e31a00c978f01f01e3cf2db466a1f46d44a4b02f
Jish2612 <167243813+Jish2612@users.noreply.github.com>
2026-02-16 23:23:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+btn.addEventListener("click",(e)=>{
+	 e.preventDefault(); 
+	const fname = document.getElementById('fname').value
+	const lname = document.getElementById('lname').value
+	const phn = document.getElementById('phn').value
+	const email = document.getElementById('email').value
+	alert(` First Name: ${fname} Last Name: ${lname} Phone Number: ${phn} Email ID: ${email}`)
+})
